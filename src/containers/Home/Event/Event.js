6771b2d95f761e1f8f21bf0f921073ae9deac5ee
namdaoduy <lenkc98@gmail.com>
2018-10-22 09:22:12 +0700

    Add files

---
+++ src/containers/Home/Event/Event.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react"
+import "./EventCSS.css"
+
+export default class Event extends Component {
+  render() {
+    return (
+      <div className="home-event col-lg-4 col-md-6">
+        <div className="event-inner">
+          <div className="event-img-ctn">
+            <img className="event-img" 
+              src={require("./../../../assets/images/google.jpg")} alt="">
+            </img>
+          </div>
+          <div className="event-des-ctn">
+            <h3 className="event-des-title">
+              [HÀ NỘI] - GOOGLE DIGITAL 4.0
+            </h3>
+            <div className="event-des-footer">
+              <div className="event-des-place">
+                HÀ NỘI
+              </div>
+              <div className="event-des-calendar">
+                <div className="calendar-month">THÁNG 10</div>
+                <div className="calendar-date">10</div>
+                <div className="calendar-wd">Thứ 6</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+import './index.css';
+import { MainRouter } from './containers/MainRouter';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<MainRouter />, document.getElementById('root'));
+registerServiceWorker();
